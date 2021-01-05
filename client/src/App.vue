4679a0efa5f83d3c7c8385cc13b5c0d9308ce329
jakub.urban <jakub.73@hotmail.com>
2021-01-05 12:32:20 +0100

    Init commit

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vu.e logo" src="./assets/logo.png" />
+  <HelloWorld v-bind:message="message" v-bind:request="request"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    var message = "Hello Vue 3.0 + Vite"
+    var request = "Request from API"
+    return {
+      request
+    }
+  },
+  methods: {
+
+  },
+  mounted(){
+    console.log('Component mounted')
+    var response = axios.get('http://localhost:5000/user/69').then((response) => {
+        console.log(response.data)
+        this.request = response.data.message
+    })
+  }
+}
+</script>
---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express()
+
+var corsOptions = {
+    origin: "http://localhost:3000"
+}
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", cors() ,(req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// simple route
+app.get("/users", cors(), (req, res) => {
+    res.json({ message: "There is no users.." });
+  });
+
+// simple route
+app.get("/user/:id", cors(), (req, res) => {
+    console.log(req.params)
+    var message = JSON.stringify(req.params.id)
+    res.json({ message: message});
+  });
+
+// set port, listen for requests
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
